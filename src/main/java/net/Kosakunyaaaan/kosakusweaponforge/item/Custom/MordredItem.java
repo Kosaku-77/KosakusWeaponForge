6586e2f470f6eeb385f2197796c4c99c8934ac21
Kosaku-77 <kosaku77@pm.me>
2025-10-18 18:42:14 +0200

    Day 2 - turned Mordred into an actual weapon !!1! :D also successfully coded an array for fetching entity positions

--- src/main/java/net/Kosakunyaaaan/kosakusweaponforge/item/Custom/MordredItem.java
+++ src/main/java/net/Kosakunyaaaan/kosakusweaponforge/item/Custom/MordredItem.java
@@ -9,17 +9,13 @@ import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.tools.picocli.CommandLine;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MordredItem extends SwordItem {
 
